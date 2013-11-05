e65433a6fd9d93e02be4ff5c8353e7c20b47b9d3
William Woodruff <woodrufw@bxscience.edu>
2013-11-05 13:55:24 -0500

    fixed method change

--- testing/ComplexTest.java
+++ testing/ComplexTest.java
@@ -7,27 +7,27 @@ public class ComplexTest
   Complex c3;
 
   c3 = ComplexMath.conjugate(c1);
-  System.out.println(c3.strRep());
+  System.out.print(c3.toString());
 
   c3 = ComplexMath.reciprocal(c1);
-  System.out.println(c3.strRep());
+  System.out.print(c3.toString());
 
   c3 = ComplexMath.add(c1, c2);
-  System.out.println(c3.strRep());
+  System.out.print(c3.toString());
 
   c3 = ComplexMath.subtract(c1, c2);
-  System.out.println(c3.strRep());
+  System.out.print(c3.toString());
 
   c3 = ComplexMath.multiply(c1, c2);
-  System.out.println(c3.strRep());
+  System.out.print(c3.toString());
 
   c3 = ComplexMath.divide(c1, c2);
-  System.out.println(c3.strRep());
+  System.out.print(c3.toString());
 
   c3 = ComplexMath.pow(c1, 3);
-  System.out.println(c3.strRep());
+  System.out.print(c3.toString());
 
   double mag = ComplexMath.mag(c1);
-  System.out.println(mag);
+  System.out.print(mag);
  }
 }
